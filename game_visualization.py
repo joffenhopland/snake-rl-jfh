7e9539e5eb679c2d923efde4e9f4633507ccfcd6
Jon-Fredrik Hopland <jho107@post.uit.no>
2023-11-22 09:00:59 -0500

    commit

--- game_visualization.py
+++ game_visualization.py
@@ -21,7 +21,7 @@ with open("model_config/{:s}.json".format(version), "r") as f:
     n_actions = m["n_actions"]
     obstacles = bool(m["obstacles"])
 
-iteration_list = [200000]
+iteration_list = [195000]
 max_time_limit = 398
 
 # setup the environment
